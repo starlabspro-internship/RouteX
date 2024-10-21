992efddd82396a57093cd1395ab219f459f89705
Engjëll Berisha <egiberisha9@gmail.com>
2024-10-21 17:44:44 +0200

    Add all wp-content files

---
+++ themes/routexTheme/templates/template-about.php
@@ -0,0 +1,9 @@
+<?php  
+/*
+Template Name: About Page
+*/
+get_header();
+?>
+
+
+<?php get_footer(); ?>
---
+++ themes/routexTheme/templates/template-pricingtables.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+Template Name: Pricing tables Page
+*/ 
+get_header();
+?>
+
+
+<?php get_footer(); ?>
