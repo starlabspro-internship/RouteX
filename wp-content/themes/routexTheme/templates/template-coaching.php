431170143cf4dd91050786d7e8570673d7cd286e
Engjëll Berisha <egiberisha9@gmail.com>
2024-10-22 13:09:23 +0200

    Merged the template files to the main branch

---
+++ wp-content/themes/routexTheme/templates/template-coaching.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+Template Name: Coaching Page
+*/
+get_header(); 
+?>
+
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/routexTheme/templates/template-countrydetails.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+Template Name: Country Details Page
+*/
+get_header(); 
+?>
+
+
+<?php get_footer(); ?>
