ed15379dafb2f5d824e56bb419c3fdc0b3e770d9
LekeMarkaj <markaj.leka@gmail.com>
2024-10-23 11:47:02 +0200

    Updated .gitignore and removed specified files from tracking

--- wp-content/themes/routexTheme/templates/template-blog.php
+++
@@ -1,9 +0,0 @@
-<?php 
-/*
-Template Name: Blog Page
-*/
-get_header(); 
-?>
-
-
-<?php get_footer(); ?>
--- wp-content/themes/routexTheme/templates/template-coachingdetails.php
+++
@@ -1,9 +0,0 @@
-<?php 
-/*
-Template Name: Coaching details Page
-*/
-get_header(); 
-?>
-
-
-<?php get_footer(); ?>
