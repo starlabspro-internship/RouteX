403460d582851b9ab10677ae6bcea383f2180dc8
LekeMarkaj <markaj.leka@gmail.com>
2024-10-28 10:59:49 +0100

    Squashed commit of the following:
    commit 8bc6b4b8535e471f6cefe51f416d64b63e2fec8b
    Merge: 2587bdb 7f029b6
    Author: LekeMarkaj <markaj.leka@gmail.com>
    Date:   Mon Oct 28 10:58:18 2024 +0100
    
        Merge branch 'main' into theme-settings-acf
    
    commit 2587bdbb2e501f74ffe6c58d5d89159cd128e743
    Author: LekeMarkaj <markaj.leka@gmail.com>
    Date:   Fri Oct 25 18:02:05 2024 +0200
    
        Added some setting in Options page
    
    commit 6187f9d47d7fb009b8d54dd7eff991cf1112ba2c
    Author: LekeMarkaj <markaj.leka@gmail.com>
    Date:   Thu Oct 24 15:28:46 2024 +0200
    
        added plugins

--- wp-content/themes/routexTheme/header.php
+++ wp-content/themes/routexTheme/header.php
@@ -17,7 +17,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
-    <?php wp_head(); ?>
+	<?php wp_head(); ?>
+
+	<?php if(get_field('dark_mode', 'options')):?>
+		<style>
+			body{
+				background: #000 !important;
+			}
+		</style>
+	<?php endif;?>
 </head>
 
 <body <?php body_class(); ?>>
