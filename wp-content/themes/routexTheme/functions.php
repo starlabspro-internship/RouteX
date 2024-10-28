b2da23bfa29f487a29f8b4b3b845e7d2b00943fc
LekeMarkaj <markaj.leka@gmail.com>
2024-10-28 15:54:36 +0100

    Implemented sass

--- wp-content/themes/routexTheme/functions.php
+++ wp-content/themes/routexTheme/functions.php
@@ -153,6 +153,14 @@ function routextheme_scripts() {
 
 	// Enqueue theme navigation script
 	wp_enqueue_script('routextheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
+
+	wp_register_style('style', get_template_directory_uri() . '/dist/app.css', [], 1, 'all');
+    wp_enqueue_style('style');
+
+	wp_enqueue_script('jquery');
+
+	wp_register_script('app', get_template_directory_uri() . '/dist/app.js', ['jquery'], 1, true);
+    wp_enqueue_script('app');
 }
 add_action( 'wp_enqueue_scripts', 'routextheme_scripts' );
 
