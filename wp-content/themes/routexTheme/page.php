493dc720c29c179d9501c446d69aa34455dd1612
LekeMarkaj <markaj.leka@gmail.com>
2024-10-30 15:04:22 +0100

    Fixed Theme structure

--- wp-content/themes/routexTheme/page.php
+++ wp-content/themes/routexTheme/page.php
@@ -22,32 +22,10 @@ get_header();
     <?php get_template_part('template-parts/client-section'); ?>
 
 
-    <?php
-		while ( have_posts() ) :
-        the_post();?>
+    
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <?php get_template_part( 'template-parts/content', 'page' ); ?>
-            </div>
-        </div>
-    </div>
-
-    <?php
-    the_post();
-    // If comments are open or we have at least one comment, load up the comment template.
-    if ( comments_open() || get_comments_number() ) :
-        comments_template();
-    endif;
-    endwhile; // End of the loop.
-	?>
-
-
-    <script src="<?php echo get_template_directory_uri(); ?>/app.js"></script>
-
-</main><!-- #main -->
+</main>
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
