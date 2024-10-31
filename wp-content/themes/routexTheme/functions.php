48fa7d0639678c0b0c18c6221a35312ba4c2ddcf
LekeMarkaj <markaj.leka@gmail.com>
2024-10-31 20:15:48 +0100

    Fixed conflicts

--- wp-content/themes/routexTheme/functions.php
+++ wp-content/themes/routexTheme/functions.php
@@ -144,6 +144,7 @@ function routextheme_scripts() {
 	
    
 	// Enqueue Swiper CSS and JS
+	wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css');
 	wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), null, true);
 
 	// // Enqueue theme navigation script
