93bd3263ea2ab8eb45c2baf6edffd09ecd62550a
Elona Beka <elonabeka3@gmail.com>
2024-11-01 13:10:54 +0100

    /

--- wp-content/themes/routexTheme/dynamic-assets/dynamic-fonts.php
+++ wp-content/themes/routexTheme/dynamic-assets/dynamic-fonts.php
@@ -1,10 +1,6 @@
 <?php
-//elona
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php' );
-//engjell
-//require_once( $_SERVER['DOCUMENT_ROOT'] . '/RouteX/wp-load.php' );
-//leka
-//require_once( $_SERVER['DOCUMENT_ROOT'] . '/RouteX/wp-load.php' );
+
 
 header("Content-type: text/css");
 
