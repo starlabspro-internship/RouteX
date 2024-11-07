3b2692bbaab00637489cae660e012406375ab649
LekeMarkaj <markaj.leka@gmail.com>
2024-11-07 17:31:09 +0100

    Fixed php structure part 2

--- wp-content/themes/routexTheme/template-parts/flexible-content/cards-section.php
+++ wp-content/themes/routexTheme/template-parts/flexible-content/cards-section.php
@@ -9,23 +9,34 @@
 
 <div class="container my-5">
     <div class="row">
-        <?php if ( have_rows('cards') ) : ?>
-            <?php while ( have_rows('cards') ) : the_row(); 
-                $card_icon = get_sub_field('card_icon');
-                $card_title = get_sub_field('card_title');
-                $card_text = get_sub_field('card_text');
-            ?>
-                <div class="col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="circle">
-                                <img src="<?php echo esc_url($card_icon); ?>" alt="Icon" class="card-icon"> <!-- Added class for icon styling -->
+        <?php if ( have_rows('offers') )  : ?>
+            
+            <?php while ( have_rows('offers') ) : the_row(); 
+                
+                if (get_row_layout() == 'offers_layout') :
+                    if (have_rows('cards')) :
+                        while (have_rows('cards')) : the_row();
+                            $card_icon = get_sub_field('card_icon');
+                            $card_title = get_sub_field('card_title');
+                            $card_text = get_sub_field('card_text');
+
+                            ?>
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="circle">
+                                            <img src="<?php echo esc_url($card_icon); ?>" alt="Icon" class="card-icon">
+                                        </div>
+                                        <h3 class="card-title"><?php echo esc_html($card_title); ?></h3>
+                                        <p class="card-text"><?php echo esc_html($card_text); ?></p>
+                                    </div>
+                                </div>
                             </div>
-                            <h3 class="card-title"><?php echo esc_html($card_title); ?></h3>
-                            <p class="card-text"><?php echo esc_html($card_text); ?></p>
-                        </div>
-                    </div>
-                </div>
+                    <?php
+                        endwhile;
+                    endif;
+                endif;
+            ?>
             <?php endwhile; ?>
         <?php else : ?>
             <p>No offers available at the moment.</p>
