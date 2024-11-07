94ecc6e4f43540f13cc992bef88f7ac71068e9b1
Engjell Berisha <egiberisha9@gmail.com>
2024-11-07 19:56:47 +0100

    Updates for: Header, Sections, ACF

--- wp-content/themes/routexTheme/template-parts/flexible-content/cards-section.php
+++ wp-content/themes/routexTheme/template-parts/flexible-content/cards-section.php
@@ -6,41 +6,42 @@
  */
 ?>
 
-
-<div class="container my-5">
-    <div class="row">
-        <?php if ( have_rows('offers') )  : ?>
-            
-            <?php while ( have_rows('offers') ) : the_row(); 
-                
-                if (get_row_layout() == 'offers_layout') :
-                    if (have_rows('cards')) :
-                        while (have_rows('cards')) : the_row();
+<?php if ( have_rows('cards') ) : ?>
+    <div class="container my-5">
+        <div class="row cardrow">
+            <?php while ( have_rows('cards') ) : the_row(); 
+                if ( get_row_layout() == 'cardlayout' ) :
+                    // Loop through the card_repeater field inside the cardlayout layout
+                    if ( have_rows('card_repeater') ) :
+                        while ( have_rows('card_repeater') ) : the_row();
                             $card_icon = get_sub_field('card_icon');
                             $card_title = get_sub_field('card_title');
                             $card_text = get_sub_field('card_text');
-
-                            ?>
+            ?>
                             <div class="col-md-4">
-                                <div class="card">
+                                <div class="card card1">
                                     <div class="card-body">
-                                        <div class="circle">
-                                            <img src="<?php echo esc_url($card_icon); ?>" alt="Icon" class="card-icon">
+                                        <div class="circle cardic">
+                                            <?php if ($card_icon) : ?>
+                                                <img src="<?php echo esc_url($card_icon); ?>" alt="Icon" class="card-icon"> <!-- Added class for icon styling -->
+                                            <?php endif; ?>
                                         </div>
-                                        <h3 class="card-title"><?php echo esc_html($card_title); ?></h3>
-                                        <p class="card-text"><?php echo esc_html($card_text); ?></p>
+                                        <?php if ($card_title) : ?>
+                                            <h3 class="card-title"><?php echo esc_html($card_title); ?></h3>
+                                        <?php endif; ?>
+                                        <?php if ($card_text) : ?>
+                                            <p class="card-text"><?php echo esc_html($card_text); ?></p>
+                                        <?php endif; ?>
                                     </div>
                                 </div>
                             </div>
-                    <?php
+            <?php 
                         endwhile;
                     endif;
                 endif;
-            ?>
-            <?php endwhile; ?>
-        <?php else : ?>
-            <p>No offers available at the moment.</p>
-        <?php endif; ?>
+            endwhile; ?>
+        </div>
     </div>
-</div>
-
+<?php else : ?>
+    <p>No offers available at the moment.</p>
+<?php endif; ?>
