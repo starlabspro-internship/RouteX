4888a623a6e0b90f5951df8525a2429877f1c2cc
LekeMarkaj <markaj.leka@gmail.com>
2024-11-07 16:44:29 +0100

    Fixed php structure

--- wp-content/themes/routexTheme/template-parts/flexible-content/available-countries-section.php
+++ wp-content/themes/routexTheme/template-parts/flexible-content/available-countries-section.php
@@ -1,3 +1,30 @@
+<?php
+    if (have_rows('available_countries')) :
+        $smalltitle = null;
+        $title = null;
+        $button = null;
+        $cards = [];
+        
+        while (have_rows('available_countries')) : the_row();
+            if (get_row_layout() == 'sector7layout1') :
+                $smalltitle = get_sub_field('smalltitle');
+                $title = get_sub_field('title');
+                $button = get_sub_field('button');
+                if (have_rows('cards')) :
+                    while (have_rows('cards')) : the_row();
+                        $cards[] = [
+                            'card_icon' => get_sub_field('card_icon'),
+                            'card_title' => get_sub_field('card_title'),
+                            'card_bullet_points' => get_sub_field('card_bullet_points')
+                        ];
+                    endwhile;
+                endif;
+            endif;
+
+        endwhile; 
+
+        if($smalltitle && $title && $button && $cards):
+?>
 <section class="available-countries-section">
     <div class="available-countries-section-container top-bottom">
         <div class="container">
@@ -6,83 +33,48 @@
                     <div class="available-countries-section-subtitles">
                             <div class="subtitle available-countries-subtitle">
                                 <?php
-                                    $available_countries = get_field('available_countries', 'options');
-
-                                    if ($available_countries) {
-                                        foreach ($available_countries as $country) {
-                                            if ($country['acf_fc_layout'] === 'sector7layout1' && isset($country['smalltitle'])) {
-                                                echo esc_html($country['smalltitle']);
-                                            }
-                                        }
-                                    }
+                                    echo esc_html($smalltitle);
                                 ?>
                             </div>
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/subtitle-icon-2.svg" alt="">
                     </div>
                     <div class="title available-countries-title">
                         <?php
-                            $available_countries = get_field('available_countries', 'options');
 
-                            if ($available_countries) {
-                                foreach ($available_countries as $country) {
-                                    if ($country['acf_fc_layout'] === 'sector7layout1' && isset($country['title'])) {
-                                        echo esc_html($country['title']);
-                                    }
-                                }
-                            }
+                            echo esc_html($title);
                         ?>
                     </div>
                 </div>
                 <div class="available-countries-section-buttons">
-                    <?php
-                        $available_countries = get_field('available_countries', 'options');
-
-                        if ($available_countries) {
-                            foreach ($available_countries as $country) {
-                                if ($country['acf_fc_layout'] === 'sector7layout1' && isset($country['button'])) {
-                                    echo '<a class="available-countries-section-button" href="' . esc_url($country['button']) . '">';
-                                    echo 'View More <img src="' . get_template_directory_uri() . '/assets/icons/right-arrow.svg" alt="">';
-                                    echo '</a>';
-                                }
-                            }
-                        }
-                    ?>
+                    <a class="available-countries-section-button" href="<?php echo esc_url($button) ?>">View More <img src="<?php echo get_template_directory_uri() . '/assets/icons/right-arrow.svg' ?>" alt="" > </a>
                 </div>
             </div>
             <div class="available-countries-section-cards">
                 <div class="row">
                     <?php
-                        $available_countries = get_field('available_countries', 'options');
-
-                        if ($available_countries) {
-                            foreach ($available_countries as $country) {
-                                if (isset($country['cards']) && is_array($country['cards'])) {
-                                    foreach ($country['cards'] as $card) {
-                                        ?>
-                                        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-                                            <div class="available-countries-item">
-                                                <div class="available-countries-item-icon">
-                                                    <!-- <img src="<?php echo esc_url($card['card_icon']); ?>" alt="Card icon"> -->
-                                                     <img src=""> 
-                                                </div>
-                                                <div class="available-countries-item-content">
-                                                    <h3><?php echo esc_html($card['card_title']); ?></h3>
-                                                    <div class="available-countries-item-content-list">
-                                                        <ul>
-                                                            <?php foreach ($card['card_bullet_points'] as $card_bullet_points) { ?>
-                                                            
-                                                                <li><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/green-checkmark.svg" alt=""><?php echo esc_html($card_bullet_points['card_text']); ?></li>
-                                                            
-                                                            <?php }?>
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </div>
+                        foreach ($cards as $card) {
+                            ?>
+                            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+                                <div class="available-countries-item">
+                                    <div class="available-countries-item-icon">
+                                        <!-- <img src="<?php echo esc_url($card['card_icon']); ?>" alt="Card icon"> -->
+                                        <img src=""> 
+                                    </div>
+                                    <div class="available-countries-item-content">
+                                        <h3><?php echo esc_html($card['card_title']); ?></h3>
+                                        <div class="available-countries-item-content-list">
+                                            <ul>
+                                                <?php foreach ($card['card_bullet_points'] as $card_bullet_points) { ?>
+                                                
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/green-checkmark.svg" alt=""><?php echo esc_html($card_bullet_points['card_text']); ?></li>
+                                                
+                                                <?php }?>
+                                            </ul>
                                         </div>
-                                        <?php
-                                    }
-                                }
-                            }
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
                         }
                     ?>
                 </div>
@@ -90,3 +82,7 @@
         </div>
     </div>
 </section>
+<?php
+        endif;
+    endif;
+?>
