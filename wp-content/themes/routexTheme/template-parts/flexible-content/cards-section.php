49633fbd8173eb7b93ab7cced7448ab6382ce60e
Engjell Berisha <egiberisha9@gmail.com>
2024-11-07 14:54:08 +0100

    Fixed media Query for Visa and Cards Section  (Mobile Style First)

--- wp-content/themes/routexTheme/template-parts/flexible-content/cards-section.php
+++ wp-content/themes/routexTheme/template-parts/flexible-content/cards-section.php
@@ -8,7 +8,7 @@
 
 
 <div class="container my-5">
-    <div class="row">
+    <div class="row cardrow">
         <?php if ( have_rows('cards') ) : ?>
             <?php while ( have_rows('cards') ) : the_row(); 
                 $card_icon = get_sub_field('card_icon');
@@ -16,9 +16,9 @@
                 $card_text = get_sub_field('card_text');
             ?>
                 <div class="col-md-4">
-                    <div class="card">
+                    <div class="card card1">
                         <div class="card-body">
-                            <div class="circle">
+                            <div class="circle cardic">
                                 <img src="<?php echo esc_url($card_icon); ?>" alt="Icon" class="card-icon"> <!-- Added class for icon styling -->
                             </div>
                             <h3 class="card-title"><?php echo esc_html($card_title); ?></h3>
