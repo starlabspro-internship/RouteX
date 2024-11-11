f431328a9e4b01f1af85f92de71a35c09342d9f9
Engjell Berisha <egiberisha9@gmail.com>
2024-11-11 16:18:52 +0100

    Fixed sections php code on the top

--- wp-content/themes/routexTheme/template-parts/flexible-content/cta-section.php
+++ wp-content/themes/routexTheme/template-parts/flexible-content/cta-section.php
@@ -1,60 +1,86 @@
+<?php
+$left_image = get_sub_field('left_image');
+$icon = get_sub_field('icon');
+$top_title = get_sub_field('top_title');
+$top_text = get_sub_field('top_text');
+$button = get_sub_field('button');
+$right_image = get_sub_field('right_image');
+
+$bottom_counters = [];
+if (have_rows('bottom_counter')) :
+    while (have_rows('bottom_counter')) : the_row();
+        $bottom_counters[] = [
+            'numbers' => get_sub_field('numbers'),
+            'title' => get_sub_field('title'),
+        ];
+    endwhile;
+endif;
+?>
+
 <div class="container cta-container">
     <div class="row">
         <!-- Left Div (30%) -->
         <div class="col-md-4 ctadiv imagediv col-12">
-            <?php 
-            // Retrieve left image
-            $left_image = get_sub_field('left_image'); 
-            if ($left_image): ?>
+            <?php if ($left_image) : ?>
                 <img src="<?php echo esc_url($left_image); ?>" class="img-fluid" alt="Left Image" />
-            <?php else: ?>
+            <?php else : ?>
                 <p>Left Image not found.</p>
             <?php endif; ?>
-
         </div>
 
         <!-- Right Div (70%) -->
         <div class="col-md-8 col-12 cta-content-div">
             <div class="top-right-section ctadiv mb-4 d-flex flex-wrap">
-            <!-- Left Content Div -->
-            <div class="left-content col-md-8 col-12 d-flex flex-column align-items-start">
-                <!-- Icon -->
-                <?php 
-                $icon = get_sub_field('icon'); 
-                if ($icon): ?>
-                    <div class="circle">
-                        <img src="<?php echo esc_url($icon); ?>" class="icon" alt="Icon" />
-                    </div>
-                <?php else: ?>
-                    <p>Icon not found.</p>
+                <!-- Left Content Div -->
+                <div class="left-content col-md-8 col-12 d-flex flex-column align-items-start">
+                    <!-- Icon -->
+                    <?php if ($icon) : ?>
+                        <div class="circle">
+                            <img src="<?php echo esc_url($icon); ?>" class="icon" alt="Icon" />
+                        </div>
+                    <?php else : ?>
+                        <p>Icon not found.</p>
+                    <?php endif; ?>
+
+                    <!-- Top Title -->
+                    <?php if ($top_title) : ?>
+                        <h2 class="title2"><?php echo esc_html($top_title); ?></h2>
+                    <?php endif; ?>
+
+                    <!-- Top Text -->
+                    <?php if ($top_text) : ?>
+                        <p><?php echo esc_html($top_text); ?></p>
+                    <?php endif; ?>
+
+                    <!-- Button -->
+                    <?php if ($button) : ?>
+                        <a href="<?php echo esc_url($button); ?>" class="button btn btn-primary">Contact us ➜</a>
+                    <?php endif; ?>
+                </div>
+
+                <!-- Right Image Div -->
+                <?php if ($right_image) : ?>
+                    <div class="right-image col-md-4 col-12" style="background-image: url('<?php echo esc_url($right_image); ?>');"></div>
                 <?php endif; ?>
-
-                <!-- Top Title -->
-                <h2 class="title2"><?php echo esc_html(get_sub_field('top_title')); ?></h2>
-
-                <!-- Top Text -->
-                <p><?php echo esc_html(get_sub_field('top_text')); ?></p>
-
-                <!-- Button -->
-                <a href="<?php echo esc_url(get_sub_field('button')); ?>" class="button btn btn-primary">Contact us ➜</a>
             </div>
 
-            <!-- Right Image Div -->
-            <div class="right-image col-md-4 col-12" style="background-image: url('<?php echo esc_url(get_sub_field('right_image')); ?>');"></div>
-        </div>
-
-        <!-- Bottom Counter Section -->
-        <?php if (have_rows('bottom_counter')): ?>
-            <div class="bottom-counter ctadiv">
-                <?php while (have_rows('bottom_counter')): the_row(); ?>
-                    <div class="counter-item">
-                        <h3><?php echo esc_html(get_sub_field('numbers')); ?></h3>
-                        <p><?php echo esc_html(get_sub_field('title')); ?></p>
-                    </div>
-                <?php endwhile; ?>
-            </div>
-        <?php endif; ?>
-
+            <!-- Bottom Counter Section -->
+            <?php if ($bottom_counters) : ?>
+                <div class="bottom-counter ctadiv">
+                    <?php foreach ($bottom_counters as $counter) : ?>
+                        <?php if ($counter['numbers'] || $counter['title']) : ?>
+                            <div class="counter-item">
+                                <?php if ($counter['numbers']) : ?>
+                                    <h3><?php echo esc_html($counter['numbers']); ?></h3>
+                                <?php endif; ?>
+                                <?php if ($counter['title']) : ?>
+                                    <p><?php echo esc_html($counter['title']); ?></p>
+                                <?php endif; ?>
+                            </div>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </div>
--- wp-content/themes/routexTheme/template-parts/flexible-content/testimonial-section.php
+++ wp-content/themes/routexTheme/template-parts/flexible-content/testimonial-section.php
@@ -1,32 +1,54 @@
+<?php
+// ACF Variables and Conditional Logic
+$testimonial_left_image = esc_url(get_sub_field('testimonial_left_image'));
+$testimonial_card = have_rows('testimonial_card');
+$testimonial_cards = [];
+$card_count = 0;
+
+// If there are testimonial cards, store them in an array for later use
+if ($testimonial_card) {
+    while (have_rows('testimonial_card')): the_row();
+        $card_count++; // Increment card count
+        $testimonial_cards[] = [
+            'text' => esc_html(get_sub_field('text')),
+            'icon' => esc_url(get_sub_field('icon')),
+            'title' => esc_attr(get_sub_field('title')),
+            'name' => esc_html(get_sub_field('name')),
+            'position' => esc_html(get_sub_field('position')),
+        ];
+    endwhile;
+}
+?>
+
 <section class="testimonial-section">
     <div class="container">
         <div class="row">
             <!-- Left Column: Image -->
-            <div class="col-12 col-md-4 testimonial-left" style="background-image: url('<?php echo esc_url(get_sub_field('testimonial_left_image')); ?>');">
+            <div class="col-12 col-md-4 testimonial-left" style="background-image: url('<?php echo $testimonial_left_image; ?>');">
             <!-- Left Div: Image -->
             </div>
 
             <!-- Right Column: Testimonial Cards -->
             <div class="col-12 col-md-8 testimonial-right d-flex flex-column justify-content-center">
-                <?php if (have_rows('testimonial_card')): ?>
+                <?php if ($testimonial_card): ?>
                     <div class="testimonial-cards">
                         <?php 
-                        $card_count = 0; // Initialize card count for navigation
-                        while (have_rows('testimonial_card')): the_row(); 
-                            $card_count++; // Increment card count
+                        $card_count = 0; // Re-initialize card count for navigation
+                        foreach ($testimonial_cards as $index => $card): 
+                            $card_count++; 
                         ?>
                             <div class="testimonial-card mb-4" id="testimonial-card-<?php echo $card_count; ?>" style="display: <?php echo $card_count === 1 ? 'block' : 'none'; ?>;">
                                 <div class="testimonial-content">
                                     <svg width="73" height="56" viewBox="0 0 73 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                     <path d="M36.8723 41.0882C36.493 40.7847 36.4172 40.4054 36.5689 39.7605L43.8519 14.5354C43.9657 14.0423 44.383 13.7009 44.914 13.7009H62.0595C62.7423 13.7009 63.0078 14.0044 63.0078 14.6492V17.2286C62.9699 17.7218 62.8181 18.177 62.5526 18.5563L43.5105 43.6676C43.2071 44.1228 42.5622 44.2745 42.0691 43.971L36.8723 41.0882ZM10.1679 41.0882C9.78857 40.7847 9.71272 40.4054 9.86444 39.7605L17.1475 14.5354C17.2613 14.0044 17.7544 13.663 18.2855 13.7009H35.3551C36.0378 13.7009 36.3034 14.0044 36.3034 14.6492V17.2286C36.2654 17.7218 36.1137 18.177 35.8482 18.5563L16.8819 43.6676C16.5785 44.1228 15.9336 44.2745 15.4405 43.9331L10.1679 41.0882Z" fill="white"/>
+                                        <path d="M36.8723 41.0882C36.493 40.7847 36.4172 40.4054 36.5689 39.7605L43.8519 14.5354C43.9657 14.0423 44.383 13.7009 44.914 13.7009H62.0595C62.7423 13.7009 63.0078 14.0044 63.0078 14.6492V17.2286C62.9699 17.7218 62.8181 18.177 62.5526 18.5563L43.5105 43.6676C43.2071 44.1228 42.5622 44.2745 42.0691 43.971L36.8723 41.0882ZM10.1679 41.0882C9.78857 40.7847 9.71272 40.4054 9.86444 39.7605L17.1475 14.5354C17.2613 14.0044 17.7544 13.663 18.2855 13.7009H35.3551C36.0378 13.7009 36.3034 14.0044 36.3034 14.6492V17.2286C36.2654 17.7218 36.1137 18.177 35.8482 18.5563L16.8819 43.6676C16.5785 44.1228 15.9336 44.2745 15.4405 43.9331L10.1679 41.0882Z" fill="white"/>
                                     </svg>
-                                    <p class="testimonial-text"><?php echo esc_html(get_sub_field('text')); ?></p>
+                                    <p class="testimonial-text"><?php echo $card['text']; ?></p>
                                     <p class="testimonial-author">
                                         <div class="testimonial-icon mb-3">
-                                            <img src="<?php echo esc_url(get_sub_field('icon')); ?>" alt="<?php echo esc_attr(get_sub_field('title')); ?>" class="img-fluid">
+                                            <img src="<?php echo $card['icon']; ?>" alt="<?php echo $card['title']; ?>" class="img-fluid">
                                             <div class="author-div">
-                                                <p class="author-name"><?php echo esc_html(get_sub_field('name')); ?></p>
-                                                <p class="author-position"><?php echo esc_html(get_sub_field('position')); ?></p>
+                                                <p class="author-name"><?php echo $card['name']; ?></p>
+                                                <p class="author-position"><?php echo $card['position']; ?></p>
                                             </div>
                                         </div>
                                     </p>
@@ -46,7 +68,7 @@
                                     </div>
                                 </div>
                             </div>
-                        <?php endwhile; ?>
+                        <?php endforeach; ?>
                     </div>
                 <?php else: ?>
                     <p>No testimonials found.</p>
@@ -55,10 +77,6 @@
         </div>
     </div>
 </section>
-
-
-
-
 
 <script>
 document.addEventListener('DOMContentLoaded', function () {
@@ -89,7 +107,4 @@ document.addEventListener('DOMContentLoaded', function () {
     // Initially show the first card
     showCard(currentCard);
 });
-
-
 </script>
-
