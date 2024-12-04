4a9de4ea87537f66b226bdd725adc52d16983034
arlindbikliqi1 <arlinndbikliqi@hotmail.com>
2024-12-04 10:59:01 +0000

    Automated commit from server changes

--- scripts/auto-push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /var/www/RouteX || exit
-git add .  # Stage all changes
-git commit -m "Automated commit from server changes" || echo "No changes to commit"
-git push origin main  # Push the changes to GitHub
