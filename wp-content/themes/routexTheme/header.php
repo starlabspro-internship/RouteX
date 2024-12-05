f8638999b8e7ede6611e0abddd6953b886d4a328
LekeMarkaj <markaj.leka@gmail.com>
2024-12-05 10:44:40 +0100

    commit test 3

--- wp-content/themes/routexTheme/header.php
+++ wp-content/themes/routexTheme/header.php
@@ -107,7 +107,11 @@ $appointment_link = get_field('appointment_link', 'options');
                     </a>
                 </div>
             </div>
-            <?php var_dump($GITHUB_DEPLOYMENT_SLUG); 
+            <?php 
+            if ( ! defined( 'GITHUB_DEPLOYMENT_SLUG' ) ) {
+                    define( 'GITHUB_DEPLOYMENT_SLUG', '112624' );
+                }
+
                 var_dump(GITHUB_DEPLOYMENT_SLUG);
             ?>
         </header>
