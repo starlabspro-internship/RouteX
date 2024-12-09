9e75f6a995f0b8ba12bd2d238ef2f4569adb164c
Elona Beka <elonabeka3@gmail.com>
2024-12-09 11:59:54 +0100

    Added swiper in Line Section

--- wp-content/themes/routexTheme/template-parts/flexible-content/line-section.php
+++ wp-content/themes/routexTheme/template-parts/flexible-content/line-section.php
@@ -1,101 +1,57 @@
-<?php if (have_rows('lines')) : ?>
+<?php
+$lines = [];
+
+if (have_rows('lines')) :
+    while (have_rows('lines')) : the_row();
+        $line_image = get_sub_field('line_image');
+        if ($line_image) {
+            $lines[] = esc_url($line_image);
+        }
+    endwhile;
+endif;
+
+$lines_to_display = array_merge($lines, $lines); 
+
+if (!empty($lines_to_display)) :
+?>
 <section class="line-section">
-    <div class="lines">
-        <div class="line-section-wrapper">
-            <?php while (have_rows('lines')) : the_row(); 
-                $line_image_url = get_sub_field('line_image');
-            ?>
-            <div class="line-item">
-                <?php if ($line_image_url) : ?>
-                <img src="<?php echo esc_url($line_image_url); ?>" alt="Line Icon" class="line-icon">
-                <?php endif; ?>
+    <div class="swiper-container line-carousel-swiper">
+        <div class="swiper-wrapper">
+            <?php foreach ($lines_to_display as $line_image) : ?>
+            <div class="swiper-slide">
+                <img src="<?php echo $line_image; ?>" alt="Line Icon" class="line-icon">
             </div>
-            <?php endwhile; ?>
+            <?php endforeach; ?>
         </div>
     </div>
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        new Swiper('.line-carousel-swiper', {
+            loop: true,
+            freeModeMomentum: false,
+            speed: 1000,
+            autoplay: {
+                delay: 0,
+                disableOnInteraction: false,
+            },
+            slidesPerView: 'auto',
+            breakpoints: {
+                1024: {
+                    slidesPerView: 12,
+                    spaceBetween: 20,
+                },
+                768: {
+                    slidesPerView: 7,
+                    spaceBetween: 11,
+                },
+                576: {
+                    slidesPerView: 5,
+                    spaceBetween: 10,
+                },
+            },
+            allowTouchMove: false,
+        });
+    });
+    </script>
 </section>
 <?php endif; ?>
-
-
-<script>
-document.addEventListener("DOMContentLoaded", function() {
-    const wrapper = document.querySelector(".line-section-wrapper");
-
-    if (wrapper) {
-        let scrollStep = 1;
-        const scrollInterval = 15;
-        let autoScrollInterval;
-
-        const initializeScroll = () => {
-            const items = Array.from(wrapper.children);
-            const wrapperWidth = wrapper.offsetWidth;
-            let contentWidth = wrapper.scrollWidth;
-
-            while (wrapper.children.length > items.length) {
-                wrapper.removeChild(wrapper.lastChild);
-            }
-
-            while (contentWidth < wrapperWidth * 2) {
-                items.forEach((item) => {
-                    const clone = item.cloneNode(true);
-                    wrapper.appendChild(clone);
-                });
-                contentWidth = wrapper.scrollWidth;
-            }
-
-            wrapper.scrollLeft = 0;
-        };
-
-        const autoScroll = () => {
-            if (wrapper.scrollWidth > wrapper.offsetWidth) {
-                wrapper.scrollLeft += scrollStep;
-
-                if (wrapper.scrollLeft >= wrapper.scrollWidth / 2) {
-                    wrapper.scrollLeft = 0;
-                }
-                requestAnimationFrame(autoScroll);
-            }
-        };
-
-        const adjustScrollSpeed = () => {
-            const screenWidth = window.innerWidth;
-
-            if (screenWidth < 768) {
-                scrollStep = 1;
-            } else if (screenWidth < 1200) {
-                scrollStep = 1;
-            } else {
-                scrollStep = 2;
-            }
-        };
-
-        const startAutoScroll = () => {
-            if (!autoScrollInterval) {
-                autoScrollInterval = setInterval(autoScroll, scrollInterval);
-            }
-        };
-
-        const stopAutoScroll = () => {
-            clearInterval(autoScrollInterval);
-            autoScrollInterval = null;
-        };
-
-        wrapper.addEventListener("touchstart", stopAutoScroll);
-        wrapper.addEventListener("touchend", startAutoScroll);
-
-        wrapper.addEventListener("mouseenter", stopAutoScroll);
-        wrapper.addEventListener("mouseleave", startAutoScroll);
-
-        initializeScroll();
-        adjustScrollSpeed();
-        startAutoScroll();
-
-        window.addEventListener("resize", () => {
-            stopAutoScroll();
-            adjustScrollSpeed();
-            initializeScroll();
-            startAutoScroll();
-        });
-    }
-});
-</script>
