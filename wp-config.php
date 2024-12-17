d630affbc23862b2fcb980556632bf2903117dd1
arlindbikliqi1 <arlinndbikliqi@hotmail.com>
2024-12-17 12:48:16 +0000

    installed gd and imagick

--- wp-config.php
+++ wp-config.php
@@ -37,7 +37,7 @@ define('WP_TEMP_DIR', ABSPATH . 'wp-content/temp/');
 define('WP_HOME', 'https://routex.devops99.pro'); 
 define('WP_SITEURL', 'https://routex.devops99.pro'); 
 define('WP_MAX_MEMORY_LIMIT', '256M');
-define('GITHUB_DEPLOYMENT_SLUG', '87e2c35');
+define('GITHUB_DEPLOYMENT_SLUG', 'f38a653');
 define('WP_MEMORY_LIMIT', '512M');
 //define( 'WP_DEBUG', true );
 //define( 'WP_DEBUG_LOG', true );
