3b14a55914c422a5db7068036c1a308391676204
Elona Beka <elonabeka3@gmail.com>
2024-12-18 17:20:52 +0100

    Fixing Team Details

--- wp-content/themes/routexTheme/dist/app.js
+++ wp-content/themes/routexTheme/dist/app.js
@@ -15,9 +15,12 @@
 /*!**********************!*\
   !*** ./src/app.scss ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nUndefined mixin.\n\u001b[34m    ╷\u001b[0m\n\u001b[34m274 │\u001b[0m \u001b[31m┌\u001b[0m \u001b[31m@include media-sd {\u001b[0m\n\u001b[34m275 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m  .team-member-image-wrapper {\u001b[0m\n\u001b[34m276 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m    flex: 0 0 440px;\u001b[0m\n\u001b[34m277 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m    height: 425px;\u001b[0m\n\u001b[34m278 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m  }\u001b[0m\n\u001b[34m279 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m\u001b[0m\n\u001b[34m280 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m  .team-details-name {\u001b[0m\n\u001b[34m281 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m    font-size: 1.8rem;\u001b[0m\n\u001b[34m282 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m  }\u001b[0m\n\u001b[34m283 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m\u001b[0m\n\u001b[34m284 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m  .contact-me-section {\u001b[0m\n\u001b[34m285 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m    padding: 20px;\u001b[0m\n\u001b[34m286 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m  }\u001b[0m\n\u001b[34m287 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m\u001b[0m\n\u001b[34m288 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m  .submit-button {\u001b[0m\n\u001b[34m289 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m    font-size: 12px;\u001b[0m\n\u001b[34m290 │\u001b[0m \u001b[31m│\u001b[0m \u001b[31m  }\u001b[0m\n\u001b[34m291 │\u001b[0m \u001b[31m└\u001b[0m \u001b[31m}\u001b[0m\n\u001b[34m    ╵\u001b[0m\n  src\\flexible-content\\team-details-section.scss 274:1  @import\n  src\\app.scss 35:9                                     root stylesheet\n    at processResult (C:\\laragon\\www\\RouteX\\wp-content\\themes\\routexTheme\\node_modules\\webpack\\lib\\NormalModule.js:889:19)\n    at C:\\laragon\\www\\RouteX\\wp-content\\themes\\routexTheme\\node_modules\\webpack\\lib\\NormalModule.js:1030:5\n    at C:\\laragon\\www\\RouteX\\wp-content\\themes\\routexTheme\\node_modules\\loader-runner\\lib\\LoaderRunner.js:400:11\n    at C:\\laragon\\www\\RouteX\\wp-content\\themes\\routexTheme\\node_modules\\loader-runner\\lib\\LoaderRunner.js:252:18\n    at context.callback (C:\\laragon\\www\\RouteX\\wp-content\\themes\\routexTheme\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at Object.loader (C:\\laragon\\www\\RouteX\\wp-content\\themes\\routexTheme\\node_modules\\sass-loader\\dist\\index.js:67:5)");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ }),
 
@@ -121,7 +124,8 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/dist/app": 0,
-/******/ 			"dist/header": 0
+/******/ 			"dist/header": 0,
+/******/ 			"dist/app": 0
 /******/ 		};
 /******/ 		
 /******/ 		// no chunk on demand loading
@@ -171,9 +175,9 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["dist/header"], () => (__webpack_require__("./src/app.js")))
-/******/ 	__webpack_require__.O(undefined, ["dist/header"], () => (__webpack_require__("./src/app.scss")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["dist/header"], () => (__webpack_require__("./src/_header.scss")))
+/******/ 	__webpack_require__.O(undefined, ["dist/header","dist/app"], () => (__webpack_require__("./src/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["dist/header","dist/app"], () => (__webpack_require__("./src/app.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["dist/header","dist/app"], () => (__webpack_require__("./src/_header.scss")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
