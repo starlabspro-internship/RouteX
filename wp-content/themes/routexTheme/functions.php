d5d74a2053f23430fa6a3d2bae0e7922877316df
LekeMarkaj <markaj.leka@gmail.com>
2024-12-18 16:14:58 +0100

    test

--- wp-content/themes/routexTheme/functions.php
+++ wp-content/themes/routexTheme/functions.php
@@ -209,10 +209,10 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-function add_google_fonts() {
-    wp_enqueue_style( 'plus-jakarta-sans', 'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap', false );
-}
-add_action( 'wp_enqueue_scripts', 'add_google_fonts');
+// function add_google_fonts() {
+//     wp_enqueue_style( 'plus-jakarta-sans', 'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap', false );
+// }
+// add_action( 'wp_enqueue_scripts', 'add_google_fonts');
 
 add_filter('acf/settings/save_json', 'my_acf_json_save_point');
 function my_acf_json_save_point($path) {
