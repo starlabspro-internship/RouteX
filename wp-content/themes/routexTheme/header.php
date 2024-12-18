7e08db4ddf7ea810abc97c0b1178a5b37e0c9d36
Engjell Berisha <egiberisha9@gmail.com>
2024-12-18 13:07:41 +0100

    font awesome removed

--- wp-content/themes/routexTheme/header.php
+++ wp-content/themes/routexTheme/header.php
@@ -14,7 +14,6 @@ $appointment_link = get_field('appointment_link', 'options');
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <?php wp_head(); ?>
     <?php if(get_field('dark_mode', 'options')): ?>
     <style>
