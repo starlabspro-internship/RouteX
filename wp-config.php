44eb504b90d3b7e7ecc502d7952dc87376986891
LekeMarkaj <markaj.leka@gmail.com>
2024-12-23 11:18:55 +0100

    test

--- wp-config.php
+++ wp-config.php
@@ -28,12 +28,12 @@
 /** The name of the database for WordPress */
 
 define('DB_NAME', 'routex_db');
-// define('DB_USER', 'routex_user');
-define('DB_USER', 'root');
-// define('DB_PASSWORD', 'password');
-define('DB_PASSWORD', '');
-// define('DB_HOST', 'localhost');
-define('DB_HOST', 'localhost:3307');
+define('DB_USER', 'routex_user');
+// define('DB_USER', 'root');
+define('DB_PASSWORD', 'password');
+// define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+// define('DB_HOST', 'localhost:3307');
 
 define('DB_CHARSET', 'utf8');
 define('WP_ALLOW_REPAIR', true);
@@ -41,14 +41,14 @@ define('WP_ALLOW_REPAIR', true);
 define('DB_COLLATE', '');
 define('WP_TEMP_DIR', ABSPATH . 'wp-content/temp/');
 
-// define('WP_HOME', 'http://routex.devops99.pro');
-// define('WP_SITEURL', 'http://routex.devops99.pro');
+define('WP_HOME', 'http://routex.devops99.pro');
+define('WP_SITEURL', 'http://routex.devops99.pro');
 
 define('WP_MAX_MEMORY_LIMIT', '256M');
 define('GITHUB_DEPLOYMENT_SLUG', '07c320f');
 
 define('WP_MEMORY_LIMIT', '512M');
-// $_SERVER['HTTPS'] = 'on';
+$_SERVER['HTTPS'] = 'on';
 
 //define( 'WP_DEBUG', true );
 //define( 'WP_DEBUG_LOG', true );
